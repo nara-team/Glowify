73b031983326d9d685ffa3455ae047386e6905d7
hakimlutfi46 <lutfihakim1606@gmail.com>
2024-08-16 18:10:21 +0700

    create navbar

---
+++ lib/app/modules/navbar/views/navbar_view.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+
+import 'package:get/get.dart';
+import 'package:glowify/app/modules/beranda/views/beranda_view.dart';
+import 'package:glowify/app/modules/berita/views/berita_view.dart';
+import 'package:glowify/app/modules/profil/views/profil_view.dart';
+import 'package:glowify/app/modules/riwayat/views/riwayat_view.dart';
+import 'package:glowify/app/theme/app_theme.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:ionicons/ionicons.dart';
+import '../controllers/navbar_controller.dart';
+
+class NavbarView extends GetView<NavbarController> {
+  const NavbarView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Obx(() => IndexedStack(
+              index: controller.tabIndex.value,
+              children: const [
+                BerandaView(),
+                RiwayatView(),
+                BeritaView(),
+                ProfilView(),
+              ],
+            )),
+      ),
+      bottomNavigationBar: Obx(() => BottomNavigationBar(
+            unselectedItemColor: Colors.grey,
+            selectedItemColor: primaryColor,
+            onTap: (index) {
+              controller.changeTabIndex(index);
+            },
+            currentIndex: controller.tabIndex.value,
+            showSelectedLabels: true,
+            showUnselectedLabels: true,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              _bottomNavigationBarItem(
+                activeIcon: Ionicons.home,
+                icon: Ionicons.home_outline,
+                label: 'Beranda',
+              ),
+              _bottomNavigationBarItem(
+                activeIcon: Ionicons.clipboard,
+                icon: Ionicons.clipboard_outline,
+                label: 'Riwayat',
+              ),
+              _bottomNavigationBarItem(
+                activeIcon: Ionicons.newspaper,
+                icon: Ionicons.newspaper_outline,
+                label: 'Berita',
+              ),
+              _bottomNavigationBarItem(
+                activeIcon: Ionicons.person,
+                icon: Ionicons.person_outline,
+                label: 'Profil',
+              ),
+            ],
+          )),
+    );
+  }
+
+  BottomNavigationBarItem _bottomNavigationBarItem({
+    required IconData activeIcon,
+    required IconData icon,
+    required String label,
+    int badgeCount = 0,
+  }) {
+    return BottomNavigationBarItem(
+      activeIcon: badgeCount > 0
+          ? badges.Badge(
+              badgeContent: Text(
+                badgeCount.toString(),
+                style: const TextStyle(color: Colors.white),
+              ),
+              position: badges.BadgePosition.topEnd(top: -10, end: -10),
+              child: Icon(activeIcon),
+            )
+          : Icon(activeIcon),
+      icon: badgeCount > 0
+          ? badges.Badge(
+              badgeContent: Text(
+                badgeCount.toString(),
+                style: const TextStyle(color: Colors.white),
+              ),
+              position: badges.BadgePosition.topEnd(top: -10, end: -10),
+              child: Icon(icon),
+            )
+          : Icon(icon),
+      label: label,
+    );
+  }
+}
