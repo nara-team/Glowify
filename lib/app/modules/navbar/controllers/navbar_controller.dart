66f30a95474e194c2553e479ad2a23a8bb331d06
pahmiudahgede <pahmilucu123@gmail.com>
2024-08-26 10:33:35 +0700

    feat: add feature for berita tutorial with API

--- lib/app/modules/navbar/controllers/navbar_controller.dart
+++ lib/app/modules/navbar/controllers/navbar_controller.dart
@@ -1,11 +1,16 @@
 import 'package:get/get.dart';
+import 'package:glowify/app/modules/tutorial/controllers/tutorial_controller.dart';
 
 class NavbarController extends GetxController {
-  //TODO: Implement NavbarController
   var tabIndex = 0.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    Get.lazyPut<TutorialController>(() => TutorialController());
+  }
 
   void changeTabIndex(int index) {
     tabIndex.value = index;
-    update();
   }
 }
